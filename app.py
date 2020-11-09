f3cb835c960485b1fde8b5da5e1fe9685b020482
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2020-11-09 01:26:38 -0600

    - Sets up all imports needed to read sqlite file

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 # Import Flask
 from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# We can view all of the classes that automap found
+# base.classes.keys()
+
+# Save references to each table
+measurement = base.classes.measurement
+station=base.classes.station
+
+
 
 # Create an app
 app=Flask(__name__)
@@ -15,11 +41,23 @@ def index():
     "     Start Dates: /api/v1.0/<start><br/><br/>"
     "     Start and End Dates: /api/v1.0/<start>/<end><br/><br/>")
 
+
+
+
+@app.route("/api/v1.0/precipitation)
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+
+    #Query dates and precipitation
+    dates_prcp=session.query(measurement.date,measurement.prcp)
+
+    session.close()
+
+    dates_prcp_list=list(np.ravel(dates_prcp))
+# @app.route("/api/v1.0/stations/<station>)
+# @app.route("/api/v1.0/tobs/<tobs>)
+
 if __name__=="__main__":
     app.run(debug=True)
-
-
-# @app.route("/api/v1.0/precipitation)
-# @app.route("/api/v1.0/stations)
-# @app.route("/api/v1.0/tobs)
-
